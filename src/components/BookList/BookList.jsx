715a9aad7ca7b446a74fe3adf55973d63b14b1d1
juan sebastian <juan.sebas-722hotmail.com>
2023-08-27 20:52:08 -0500

    BookList component logic implemented

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -9,7 +9,17 @@ import "./BookList.css";
 //https://covers.openlibrary.org/b/id/240727-S.jpg
 
 const BookList = () => {
+    const {books, loading, resultTitle} = useGlobalContext();
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      // removing /works/ to get only id
+      id: (singleBook.id).replace("/works/", ""),
+      cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : coverImg
+    }
+  });
 
+  if(loading) return <Loading />;
 
 }
 
