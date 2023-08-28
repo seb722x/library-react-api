500e5034f05255e6760681813de9d062a765a8ee
juan sebastian <juan.sebas-722hotmail.com>
2023-08-27 20:13:00 -0500

    index.js structured

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import BookDetails from './components/BookDetails/BookDetails';
+import BookList from './components/BookList/BookList';
+import { AppProvider } from './context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+);
