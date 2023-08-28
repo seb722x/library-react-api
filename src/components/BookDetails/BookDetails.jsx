4f7fdd945653981b0d859c156e0664cc927ab7d7
juan sebastian <juan.sebas-722hotmail.com>
2023-08-27 20:48:38 -0500

    BookDetails html added

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -47,6 +47,43 @@ const BookDetails = () => {
   }, [id]);
 
   if(loading) return <Loading />;
+
+  return (
+    <section className='book-details'>
+      <div className='container'>
+        <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
+          <FaArrowLeft size = {22} />
+          <span className='fs-18 fw-6'>Go Back</span>
+        </button>
+
+        <div className='book-details-content grid'>
+          <div className='book-details-img'>
+            <img src = {book?.cover_img} alt = "cover img" />
+          </div>
+          <div className='book-details-info'>
+            <div className='book-details-item title'>
+              <span className='fw-6 fs-24'>{book?.title}</span>
+            </div>
+            <div className='book-details-item description'>
+              <span>{book?.description}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subject Places: </span>
+              <span className='text-italic'>{book?.subject_places}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subject Times: </span>
+              <span className='text-italic'>{book?.subject_times}</span>
+            </div>
+            <div className='book-details-item'>
+              <span className='fw-6'>Subjects: </span>
+              <span>{book?.subjects}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default BookDetails
