6349bab8b7543b4c5956d674f45f9ad7879484e1
juan sebastian <juan.sebas-722hotmail.com>
2023-08-27 20:46:22 -0500

    BookDetails structured

---
+++ src/components/BookDetails/BookDetails.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import Loading from "../Loader/Loader";
+import coverImg from "../../images/cover_not_found.jpg";
+import "./BookDetails.css";
+import {FaArrowLeft} from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
+const BookDetails = () => {
+
+}
+
+export default BookDetails
