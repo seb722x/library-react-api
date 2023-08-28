1d9dce522dfb02077aba89f3a4533ab2ee589972
juan sebastian <juan.sebas-722hotmail.com>
2023-08-27 20:45:21 -0500

    BookList Added

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useGlobalContext } from '../../context';
+import Book from "../BookList/Book";
+import Loading from "../Loader/Loader";
+import coverImg from "../../images/cover_not_found.jpg";
+import "./BookList.css";
+
+const BookList = () => {
+
+
+}
+
+export default BookList
